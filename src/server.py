55c211f9ddc37f1df54bb022fc23ecce3e9e93c6
Isacescu Maxim <isacescu.maxim12@gmail.com>
2025-03-21 14:57:23 +0200

    refactor

--- src/server.py
+++ src/server.py
@@ -7,8 +7,9 @@ app = Flask(__name__)
 @app.route('/api/v1/plot-lab', methods=['POST'])
 def generate_image():
     content = request.data.decode('utf-8')
-    if not content:
-        return "No text provided", 400
+    print(content)
+    # if not content:
+    #     return "No text provided", 400
 
     # image_path = create_chart(content)
     image_path = "data/img/example.png"
