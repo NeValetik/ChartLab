544591c9dc2262afcfff123f9eea68cd0ac3d9ef
Isacescu Maxim <isacescu.maxim12@gmail.com>
2025-03-21 09:59:54 +0200

    Add endpoint for templates

--- src/server.py
+++ src/server.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, send_file
+from flask import Flask, request, send_file, jsonify
+import os
 from main import create_chart
 
 app = Flask(__name__)
@@ -13,5 +14,20 @@ def generate_image():
     image_path = "data/img/example.png"
     return send_file(image_path, mimetype='image/png')
 
+@app.route('/api/v1/files', methods=['GET'])
+def list_files():
+    repo_path = "data/templates"
+    if not os.path.exists(repo_path):
+        return "Repository not found", 404
+    
+    files_content = {}
+    for filename in os.listdir(repo_path):
+        file_path = os.path.join(repo_path, filename)
+        if os.path.isfile(file_path):
+            with open(file_path, 'r', encoding='utf-8') as file:
+                files_content[filename] = file.read()
+    
+    return jsonify(files_content)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
